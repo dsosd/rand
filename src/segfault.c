072643647bd2e2e2917452743527f1072fcc8967
Andy Huang <andy@{domain}>
2020-11-11 18:33:17 -0500

    Add snippet for intentional segfault generation
    No headers are required. It is not _guaranteed_ to produce a segfault, but will likely do so.
    
    Co-authored-by: Andy Huang <ahuang-noreply@protonmail.com>

---
+++ src/segfault.c
@@ -0,0 +1,19 @@
+//debug functions to exit with extreme prejudice
+void crash_and_burn_recurv(int* frame_num){
+	//attempt to write to address 0
+	*(char*)((long)frame_num & ~(long)frame_num) = 0;
+
+	//try to write to read-only memory
+	char* str = "";
+	str[0] = '\x01';
+
+	//infinite recursion
+	int new_num = ++*frame_num;
+	crash_and_burn_recurv(&new_num);
+}
+
+void crash_and_burn(void){
+	int init_frame_num = 0;
+	crash_and_burn_recurv(&init_frame_num);
+	exit(1);
+}
